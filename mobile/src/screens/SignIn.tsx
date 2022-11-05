ff6bdd18ea47adcf538cc38212f214c1d12380ea
B-Schmitz <bernardoschmitz0@gmail.com>
2022-11-05 15:05:44 -0300

    add aula01

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar";
+import { Center, Text } from "native-base";
+export function SignIn() {
+
+  return (
+      <Center flex={1} bgColor={"gray.900"}>
+        <Text color={"white"} fontSize={20}>
+          SignIn
+        </Text>
+        <StatusBar style="auto" />
+      </Center>
+  );
+}
+
