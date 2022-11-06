64186f36a424d701a15795314bd9dfe7283536c3
B-Schmitz <bernardoschmitz0@gmail.com>
2022-11-06 17:16:29 -0300

    add aula03

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,6 +9,10 @@ import {
 import { THEME } from "./src/styles/theme";
 import { Loading } from "./src/components/Loading";
 import { SignIn } from "./src/screens/SignIn";
+import {AuthContextProvider } from "./src/contexts/AuthContext";
+import { New } from "./src/screens/New";
+import { Find } from "./src/screens/Find";
+import { Pools } from "./src/screens/Pools";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,12 +23,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <AuthContextProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
-      />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        />
+        {fontsLoaded ? <Pools /> : <Loading />}
+        </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
